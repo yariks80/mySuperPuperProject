8d1d54466901bd98f820aee598e6e34f8a9b1558
yarik <yariks@i.ua>
2019-02-16 13:01:08 +0200

    aa

--- src/test/java/com/it/MyTest.java
+++ src/test/java/com/it/MyTest.java
@@ -24,15 +24,17 @@ public class MyTest {
     }
 
 
-
     @Test
-    public void testMyTest(String url) {
+    public void testMyTest() {
   /*    validLogin=ittest2
         validPassword=337774a
         validEmail=ittest2@i.ua*/
-   driver.get("https://www.i.ua/");
-   driver.findElement(By.name("login")).sendKeys("ittest2");
-   driver.findElement(By.name("login")).sendKeys("ittest2");
+        driver.get("https://www.i.ua/");
+        driver.findElement(By.name("login")).sendKeys("ittest2");
+        driver.findElement(By.name("pass")).sendKeys("337774a");
+        driver.findElement(By.xpath("//input[@tabindex='5']")).click();
+        String email = driver.findElement(By.xpath("//span[@class='sn_menu_title']")).getText();
+        Assert.assertEquals(email, "ittest2@i.ua");
 
     }
 }
